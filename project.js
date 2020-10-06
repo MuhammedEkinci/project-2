c2baa6888e5c4ae6a20aa8a58648680a3ee88b7e
cssmith07060 <66744380+cssmith07060@users.noreply.github.com>
2020-10-05 23:08:58 -0400

    Add files via upload

--- project.js
+++ project.js
@@ -1,30 +1,39 @@
-let topTextInput, bottomTextInput, imageInput, generateBtn, canvas, ctx;
+let topTextInput, bottomTextInput, topTextSizeInput, bottomTextSizeInput, imageInput, generateBtn, canvas, ctx;
 
-function generateMeme() {
+function generateMeme(img, topText, bottomText, topTextSize, bottomTextSize) {
+    let fontSize;
     canvas.width = img.width;
     canvas.height = img.height;
     ctx.clearRect(0, 0, canvas.height);
     ctx.drawImage(img, 0, 0);
 
-    let fontSize = canvas.width / 15;
+    //Top text font size
+    fontSize = canvas.width * topTextSize;
     ctx.font = fontSize + 'px Impact';
     ctx.fillStyle = 'white';
     ctx.strokeStyle = 'black';
-    ctx.lineWidth = fontSize / 15;
+    ctx.lineWidth = fontSize / 20;
     ctx.txtAlign = 'center';
+
+    // Draw top text
     ctx.textBaseline = 'top';
-
-    ctx.fillText(topText, canvas.width / 2, canvas.height, canvas.width);
-
+    topTextInput.split('\n').forEach(function (t, i) {
+        ctx.strokeText(bottomText, canvas.width / 2, i * fontSize, canvas.width);
+        ctx.fillText(topText, canvas.width / 2, i * fontSize, canvas.width);
+    });
+    
+    // Draw Bottom text
+    fontSize = canvas.width * bottomTextSize;
     ctx.textBaseline = "bottom";
-    ctx.FillText(bottomtext, canvas.width / 2, canvas.height);
-    ctx.fillText(bottomtext, canvas.width / 2, canvas.height);
+    bottomText.split('\n').forEach(function (t, i) {
+        ctx.FillText(bottomtext, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
+        ctx.strokeText(bottomtext, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
 
-}
+    });
 
 function init() {
-    topTextInput = document.getElementById('top-text');
-    bottomTextInput = document.getElementById('bottomtext');
+    topTextSizeInput = document.getElementById('top-text-size-input');
+    bottomTextSizeInput = document.getElementById('bottom-text-size-input');
     imageInput = document.getElementById('image-input');
     generateBtn = document.getElementById('meme.canvas');
     canvas = document.getElementById('meme-canvas')
@@ -36,7 +45,7 @@ function init() {
         reader.onload = function () {
             let img = new Image;
             img.src = reader.result;
-            generateMeme(img, topTextInput.value, bottomTextInput.value);
+            generateMeme(img, topTextInput.value, bottomTextInput.value, topTextSizeInput.value, bottomTextSizeInput.value);
 
         };
         reader.readAsDataURL(imageInput.files[0]);
