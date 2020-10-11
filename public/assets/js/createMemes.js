ab5947b6d88384abdc09b7ef986d6cb0ae415658
Muhammed Ekinci <siraceddinekinci@gmail.com>
2020-10-10 23:08:49 -0400

    gets meme url when img is clicked

--- public/assets/js/createMemes.js
+++ public/assets/js/createMemes.js
@@ -1,36 +1,40 @@
 $(document).ready(function() {
     let memeURL = "https://api.imgflip.com/get_memes";
 
-    $(document).on('click','.meme-img', doSomething)
-
     fetch(memeURL)
     .then(data => data.json())
     .then(json => {
       console.log(json);
+        
+        for(var i = 0; i < 30; i++) {
 
-      const memes = json.data.memes;
-  
-      for(var i = 0; i < 30; i++) {
-        //stores all memes retrieved form API
-        var popMemesDiv = $("<a class = 'meme-display col-lg-2' >");
-  
-  
-        //get meme img from API
-        var memeIMG = json.data.memes[i].url;
-        console.log(memeIMG);
-  
-        //creates div to store memesIMG
-        var img = $("<img>").addClass("meme-img").attr("src", memeIMG).width(100).height(100);
-        popMemesDiv.append(img);
-  
-  
-        //prepend to memes div
-        $(".meme-choices").prepend(popMemesDiv);
-      }
+            //stores all memes retrieved form API
+            var popMemesDiv = $("<a class = 'meme-display col-lg-2'>");
+    
+    
+            //get meme img from API
+            var memeIMG = json.data.memes[i].url;
+            console.log(memeIMG);
+
+             //variabel to hold meme data
+             var meme = json.data.memes[i].url;
+    
+            //creates div to store memesIMG
+            var img = $("<img>").addClass("meme-img").attr("src", memeIMG).width(100).height(100);
+            img.attr("data-url", meme);
+            popMemesDiv.append(img);
+    
+    
+            //prepend to memes div
+            $(".meme-choices").prepend(popMemesDiv);
+
+        }
     })
     .catch(error => console.log(error));
 
-    function doSomething() {
-        console.log("message");
-    }
+       //when img is clicked console.log the url to image
+       $(document).on('click','.meme-img', function() {
+            console.log($(this).attr("data-url"));
+       });
+
   });
